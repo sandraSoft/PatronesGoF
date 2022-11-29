f6a368c26ded04016ea4759c33faab18303bbd91
Sandra Hurtado <sandra.hurtado@ucaldas.edu.co>
2022-11-28 20:56:25 -0500

    Quitar nota de supress warnings en CarroException

--- src/state/solucion/CarroException.java
+++ src/state/solucion/CarroException.java
@@ -4,9 +4,8 @@ package state.solucion;
  * Excepción para indicar problemas al acelerar o frenar un carro
  * (por ejemplo, que trate de exceder la velocidad máxima).
  * 
- * @version 1.0
+ * @version 1.1
  */
-@SuppressWarnings("serial")
 public class CarroException extends Exception {
 
 	public CarroException(String mensaje) {
